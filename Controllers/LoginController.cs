c8abf20e02ab0986b3697a76b74ebdc502ba19cf
Qasim Sarwar <61008829+qasim-sarwar@users.noreply.github.com>
2022-03-04 19:16:43 +0900

    implemented swagger with JWT

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Authorization_for_Swagger.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Authorization_for_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+
+        public  IActionResult Login (Login loginDTO)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(loginDTO.UserName) || string.IsNullOrEmpty(loginDTO.Password))
+                    return BadRequest("UserName and/or Password Not specified!");
+
+                if (loginDTO.UserName.Equals("qasim-sarwar@outlook.com") &&
+               loginDTO.Password.Equals("IdevelopApps"))
+                {
+                    var secretKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes("thisisasecretkey@123"));
+                    var signinCredentials = new SigningCredentials
+                   (secretKey, SecurityAlgorithms.HmacSha256);
+                    var jwtSecurityToken = new JwtSecurityToken(
+                        issuer: "ABCXYZ",
+                        audience: "http://localhost:51398",
+                        claims: new List<Claim>(),
+                        expires: DateTime.Now.AddMinutes(10),
+                        signingCredentials: signinCredentials
+                    );
+                    Ok(new JwtSecurityTokenHandler().
+                    WriteToken(jwtSecurityToken));
+                }
+
+            }
+            catch
+            {
+                return BadRequest
+                ("An error occurred in generating the token");
+            }
+            return Unauthorized();
+        }
+
+    }
+}
---
+++ ViewModel/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization_for_Swagger.ViewModel
+{
+    public class Login
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
